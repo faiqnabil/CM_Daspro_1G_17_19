8beb9d410fad2cedbc8685b081184c48f7b874f9
SkyWarLimit <ihsanbinrahmatw@gmail.com>
2024-12-06 13:53:59 +0700

    Case_Method

--- Case_Method/CMKafe_17_19.java
+++ Case_Method/CMKafe_17_19.java
@@ -1,12 +1,13 @@
+package Case_Method;
+
 import java.util.Scanner;
 
 public class CMKafe_17_19 {
-
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String namaPelanggan, namaMenu;
         int nomorMeja, jumlahItem;
 
-        
     }
 }
